cf40a9d414f9066315de9f7b540ba243b14b0d3e
Vadym Kyrychuk <vadkirichuk@gmail.com>
2022-08-01 21:21:03 +0300

    'com'

--- pars.py
+++ pars.py
@@ -16,12 +16,23 @@ for i in range(1, pagination + 1):
     try:
         ls_links_phones = bs_page.find('div', class_='products-layout__container products-layout--grid').find_all('div',
                                                                                                                   class_='product-card__img')
-        for link in ls_links_phones: # get links phone
+        for link in ls_links_phones:  # get links phone
             temp_result = link.find('a', class_='img-carousel').get('href')
             if temp_result:
                 links_phone.append(temp_result)
     except Exception:
         print('Error')
 
+result = []
+
+# li = ['https://allo.ua/ua/products/mobile/xiaomi-black-shark-4-6-128gb-mirror-black-global.html', 'https://allo.ua/ua/products/mobile/xiaomi-redmi-note-10-pro-6-128-onyx-gray-m2101k6g_2.html']
 for phone in links_phone:
-    print(phone)
+    req_phone = requests.get(phone)
+    bs_phone = BS(req_phone.text, 'lxml')
+    title = bs_phone.find('h1', class_='p-view__header-title').text
+    picture = bs_phone.find('picture', class_='main-gallery__link').find('img').get('src')
+    sum = bs_phone.find('span', class_='sum').text.replace('\xa0', ' ').strip()
+    smphone = {'title': title, 'picture': picture, 'sum': sum.strip()}
+    result.append(smphone)
+
+print(result)
